67e3b08b6927d1838a6536098594936600c231e8
kpapezova <papezka.kristyna@gmail.com>
2023-05-14 13:41:47 +0200

    New tests for testing __init__ method were added. Testing file named test_list.csv was created.

--- test_mentee.py
+++ test_mentee.py
@@ -5,37 +5,32 @@ def test_import_Mentee():
     from mentee import Mentee
 
 
-test_data = [{'index': 1, 'first_name': 'Lacy', 'last_name': 'Keefe', 'language': 'Romanian'}, {'index': 2, 'first_name': 'Luisa', 'last_name': 'Nagy', 'language': 'English'}, {'index': 3, 'first_name': 'Kristina', 'last_name': 'Pfeifer', 'language': 'Romanian'}, {'index': 4, 'first_name': 'Lars', 'last_name': 'Greene', 'language': 'Romanian'}]
+test_csv = "test_list.csv"
+users = Mentee(test_csv)
 
-test_names = ['Lacy Keefe', 'Luisa Nagy', 'Kristina Pfeifer', 'Lars Greene']
 
 class TestMentee():
     
-    def test_count_mentees(self):
-        "Return the number of mentees"
-        mt = Mentee()
-        assert mt.count_mentees(test_data) == 4
+    def test_initial_value(self):
+        """Testing of __init__ method"""
+        assert users.num_of_mentees == 5
+        assert users.languages == {'English', 'Romanian'}
+        assert users.full_names[0] == 'Lacy Keefe'
+        assert users.full_names[1] == 'Luisa Nagy'
+        assert users.full_names[3] != 'Aileen Olvera'
+        assert users.full_names[4] == 'Aileen Olvera'
 
-    def test_spoken_languages(self):
-        "Return a set of spoken languages"
-        mt = Mentee()
-        assert mt.spoken_languages(test_data) == {'Romanian', 'English'}
 
-    def test_prep_fullname(self):
-        "Return a dict with index as a key and full name as user"
-        mt = Mentee()
-        assert mt.prep_fullname(test_data) == test_names
+    # def test_get_average(self):
+    #     "Return an average length of metees full names"
+    #     mt = Mentee()
+    #     assert mt.get_average(test_names) == 11
 
-    def test_get_average(self):
-        "Return an average length of metees full names"
-        mt = Mentee()
-        assert mt.get_average(test_names) == 11
+    # def test_get_longest(self):
+    #     "Return a longest full name"
+    #     mt = Mentee()
+    #     assert mt.get_longest(test_names) == (15, ['Kristina Pfeifer'])
 
-    def test_get_longest(self):
-        "Return a longest full name"
-        mt = Mentee()
-        assert mt.get_longest(test_names) == (15, ['Kristina Pfeifer'])
-
-    def test_get_shortest(self):
-        mt = Mentee()
-        assert mt.get_shortest(test_names) == (9, ['Lacy Keefe', 'Luisa Nagy'])
+    # def test_get_shortest(self):
+    #     mt = Mentee()
+    #     assert mt.get_shortest(test_names) == (9, ['Lacy Keefe', 'Luisa Nagy'])
